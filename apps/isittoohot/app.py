1059e5abe5a84f1885c2dd3a6f8aa4ff446ade2f
Will Wilson <will@willwilson.uk>
2026-07-06 16:11:54 +0100

    feat: add verdict and colour lookup table

--- apps/isittoohot/app.py
+++ apps/isittoohot/app.py
@@ -4,6 +4,22 @@ from events.input import Buttons, BUTTON_TYPES
 from tildagonos import tildagonos
 
 
+def _get_state(temp):
+    if temp >= 34: return "WE'RE ALL GONNA DIE", (1.0, 0.0, 0.0), (255,   0,   0)
+    if temp >= 33: return "DEAR GOD YES",        (1.0, 0.0, 0.0), (255,   0,   0)
+    if temp >= 32: return "ABSOLUTELY YES",       (1.0, 0.0, 0.0), (255,   0,   0)
+    if temp >= 31: return "OH GOD YES",           (1.0, 0.0, 0.0), (255,   0,   0)
+    if temp >= 30: return "SEND HELP",            (0.9, 0.1, 0.0), (230,  25,   0)
+    if temp >= 29: return "YEAH IT IS",           (0.8, 0.2, 0.0), (200,  50,   0)
+    if temp >= 28: return "OH YES",               (0.8, 0.2, 0.0), (200,  50,   0)
+    if temp >= 27: return "YES",                  (0.7, 0.3, 0.0), (180,  80,   0)
+    if temp >= 26: return "yes...",               (0.8, 0.5, 0.0), (200, 130,   0)
+    if temp >= 25: return "a bit yeah",           (0.9, 0.7, 0.0), (220, 170,   0)
+    if temp >= 21: return "NO",                   (0.0, 0.6, 0.1), (  0, 150,  25)
+    if temp >= 18: return "no",                   (0.0, 0.5, 0.1), (  0, 120,  25)
+    return             "ABSOLUTELY NOT",          (0.0, 0.4, 0.2), (  0, 100,  50)
+
+
 class IsItTooHotApp(app.App):
     def __init__(self):
         super().__init__()
